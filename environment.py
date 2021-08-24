7d7fd70193d00b76188d170992a0ea15ee0e2fa1
sanimatie <sanimaties@gmail.com>
2021-08-24 10:02:13 +0200

    initiële kopie

---
+++ environment.py
@@ -0,0 +1,13 @@
+# import logging
+
+# logformat="%(levelname)s:%(filename)s:%(lineno)d:%(asctime)s:%(message)s"
+# logging.basicConfig(filename='logboek.log',level=logging.DEBUG,format=logformat)
+
+# def before_all(context):
+#     logging.info('Automated testing has started')
+
+# def before_step(context, step):
+#     logging.info("Step " + step.name + " has started")
+
+# def after_all(context):
+#     logging.info('Automated testing has finished')
